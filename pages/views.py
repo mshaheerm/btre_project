12381ae2de57f98e6d03d22c00270b0e74f1c02a
mshaheerm <mshaheermunir@gmail.com>
2020-07-21 13:11:19 +0200

    Updated About Page dynamically

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-listDate').filter(isPublished=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hireDate')
+
+    # Get MVP realtor/realtors
+    mvpRealtors = Realtor.objects.all().filter(isMVP=True)
+
+    context = {
+        'realtors': realtors,
+        'mvpRealtors': mvpRealtors
+    }
+    return render(request, 'pages/about.html', context)
