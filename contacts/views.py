de4e1eddcc52e66e8d0aff88d4309962228a54a6
mshaheerm <mshaheermunir@gmail.com>
2020-07-23 12:33:27 +0200

    Contact form prep & submission

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 def contact(request):
-    return
+    if request.method == 'POST':
+        print('Hello')
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+
+    contact = Contact(listing=listing, listingID=listing_id, name=name, email=email, phone=phone, message=message, userID=user_id)
+
+    contact.save()
+
+    messages.success(request, 'Thank you for submitting your request. We will get back to you soon !')
+
+    return redirect('/listings/' + listing_id)
+
+
